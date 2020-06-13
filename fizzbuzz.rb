f08521eff3782d4608c1a051ba4f8c94700fbb9b
james wu <jwu_nyc@yahoo.com>
2020-06-13 16:50:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz (int)
   if int%3==0 && int%5==0  # if the number int is divisible by 3
     "FizzBuzz"
-  elsif int%3==0
-    "Fizz"
-  else int%5==0
-    "Buzz"
+#  elsif int%3==0
+#    "Fizz"
+#  else int%5==0
+#    "Buzz"
   end
 end
